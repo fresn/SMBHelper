7406c0f162de07687230940ca26b4f88d0e5faf1
YimingMa <emen.fresn.ma@gmail.com>
2019-03-15 10:58:30 -0500

    init running

---
+++ app/src/main/java/net/yimingma/smbhelper/MainActivity.java
@@ -0,0 +1,48 @@
+package net.yimingma.smbhelper;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.MenuItem;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    private String TAG="MainActivity";
+
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            switch (item.getItemId()) {
+                case R.id.navigation_setting:
+                    return true;
+                case R.id.navigation_dashboard:
+                    Log.d(TAG, "onNavigationItemSelected: ");
+                    return true;
+                case R.id.navigation_process:
+                    Toast.makeText(getApplicationContext(),"You don't have any process yet",Toast.LENGTH_LONG).show();
+                    return false;
+            }
+            return true;
+        }
+    };
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //set default selected item
+        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+        navigation.setSelectedItemId(R.id.navigation_dashboard);
+
+    }
+
+}
---
+++ app/src/main/java/net/yimingma/smbhelper/SettingNavFragment.java
@@ -0,0 +1,29 @@
+package net.yimingma.smbhelper;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class SettingNavFragment extends Fragment {
+
+
+    public SettingNavFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_setting_nav, container, false);
+    }
+
+}
